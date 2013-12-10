91c8101b8c8f9396be16f5dc9c205d36c1118d0e
skyblue <ssx205@gmail.com>
2013-12-10 23:37:39 +0800

    add intial project

---
+++ main.go
@@ -0,0 +1,28 @@
+// testmartini project main.go
+package main
+
+import (
+	"fmt"
+
+	"github.com/codegangsta/martini"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	m := martini.Classic()
+
+	m.Get("/:name", func(params martini.Params) string {
+		return "Hello " + params["name"]
+	})
+
+	m.Get("/", func() string {
+		return "this is index.html"
+	})
+
+	m.Get("/posts/:id", func(params martini.Params) string {
+		id := params["id"]
+		return id
+	})
+
+	m.Run()
+}
---
+++ models_test.go
@@ -0,0 +1,10 @@
+// models_test.go
+package main
+
+import (
+	"testing"
+)
+
+func TestPostAdd(t *testing.T) {
+	t.Log("Hello Add")
+}
